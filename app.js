70cfa08bef7325c385767a0bc01a13744ef9838b
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-05-23 19:41:33 -0400

    fix: edit names of identifiers and classes to semantic names according to rule 3.2 HTML & CSS Basic Level in html-and-css.md

--- app.js
+++ app.js
@@ -77,10 +77,10 @@ var editTask = function () {
   var editInput = listItem.querySelector("input[type=text]");
   var label = listItem.querySelector("label");
   var editBtn = listItem.querySelector(".edit");
-  var containsClass = listItem.classList.contains("edit-mode");
-  //If class of the parent is .editmode
+  var containsClass = listItem.classList.contains("todo__item_edit");
+  //If class of the parent is .etodo__item_edit
   if (containsClass) {
-    //switch to .edit-mode
+    //switch to .todo__item_edit
     //label becomes the inputs value.
     label.innerText = editInput.value;
     editBtn.innerText = "Edit";
@@ -89,8 +89,8 @@ var editTask = function () {
     editBtn.innerText = "Save";
   }
 
-  //toggle .editmode on the parent.
-  listItem.classList.toggle("edit-mode");
+  //toggle .todo__item_edit on the parent.
+  listItem.classList.toggle("todo__item_edit");
 };
 
 //Delete task.
